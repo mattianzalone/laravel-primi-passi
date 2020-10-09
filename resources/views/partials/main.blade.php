1c24ab037945479f3e02340eff8b34687309c1ae
mattia <mattia24.studio@gmail.com>
2020-10-09 10:08:21 +0200

    added

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,30 +1,24 @@
 <main>
     <h2>Le Lunghe</h2> 
     <div class="cards">
-        
         @foreach ($lunghe as $card)
             <div class="card">
-                <h3>{{ $card['titolo'] }}</h3>
                 <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}">
             </div>
         @endforeach
     </div>
     <h2>Le Corte</h2>
     <div class="cards">    
-        
         @foreach ($corte as $card)
             <div class="card">
-                <h3>{{ $card['titolo'] }}</h3>
                 <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}">
             </div>
         @endforeach
     </div>
     <h2>Le Cortissime</h2>
     <div class="cards">    
-        
         @foreach ($cortissime as $card)
             <div class="card">
-                <h3>{{ $card['titolo'] }}</h3>
                 <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}">
             </div>
         @endforeach
