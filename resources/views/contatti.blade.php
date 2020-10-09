36717eb6efb6cee28a1d60d9498dc988aebdb608
mattia <mattia24.studio@gmail.com>
2020-10-09 12:31:37 +0200

    added

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Contatti
-</body>
-</html>
+@extends('layout.app')
+
+@section('titolo',"info-contatti")
+    
+@section('main')
+    <div>
+        <ul>
+            <li><a href="">Chi Siamo</a></li>
+            <li><a href="">Lavora con noi</a></li>
+            <li><a href="">La Nostra Storia</a></li>
+        </ul>
+    </div>    
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    HOME
-</body>
-</html>
+@extends('layout.app')
+
+@section('titolo',"Home")
+    
+@section('main')
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, laboriosam. Placeat autem illum accusamus voluptate dolorum necessitatibus nam incidunt, sint voluptatem nulla magnam eveniet, molestiae iure doloremque aut dolor saepe?</p>
+    </div>
+@endsection
